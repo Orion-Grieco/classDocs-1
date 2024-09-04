8e4edf5002bd4674c5a84dadbceaa5b610137df6
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2024-09-04 14:49:01 -0400

    testing R in VSCode
    Testing out R add-ons and IDLE in VSCode; works well so far (Just seems to only execute with CTRL+SHIFT+S besides clicking the buttons to run code)

--- scratchpad/temp.r
+++ scratchpad/temp.r
@@ -1,1 +1,6 @@
 print("hello")
+
+#plot(cars)
+#plot(mtcars$wt, mtcars$mpg)
+library(ggplot2)
+plot(ggplot(mtcars, aes(x = wt, y = mpg)) + geom_point())
---
+++ scratchpad/temp2.r
@@ -0,0 +1,2 @@
+data_set <- seq(1, 100)
+plot(data_set)
