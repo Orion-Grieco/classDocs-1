34c6f86915c077c62f1f13d56b60e6243c6e5b0b
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2024-09-08 21:46:36 -0400

    Add w3

---
+++ lessons/03/sandbox/sunSpotAnalysis.R
@@ -0,0 +1,27 @@
+
+rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
+#Load library
+library(tidyverse)
+
+# find your sandbox file
+sunData <- read.table(file.choose(), header = TRUE, sep = ",")
+
+# See what the data looks like
+View(sunData)
+# Plot the data:
+ggplot(data = sunData) +  geom_point(
+    mapping =aec(
+      x = fracOfYear,
+      y = sunspotNum,
+      color = numObs,
+      size = year
+      )
+    )
+
+data()
